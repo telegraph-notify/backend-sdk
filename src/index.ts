bd4bdaf14dbdd5ca203e4fbd66489681cda95309
Erin <rosemary.erin.o@gmail.com>
2024-11-04 17:33:13 -0500

    feature: api request to retrieve all notification logs

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ to easily interact with the server for common tasks such as:
   -etc.
 
 */
-import axios from "axios";
 import crypto from "node:crypto";
 
 class BackendSDK {
@@ -149,6 +148,28 @@ class BackendSDK {
       return { status: response.status, message };
     } catch (error) {
       return { status: 400, message: "error" };
+    }
+  }
+
+  async getNotificationLogs() {
+    try {
+      const url = this.baseUrl + `/notification-logs`;
+
+      let response = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: this.secretKey,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching notification logs:", error);
     }
   }
 }
